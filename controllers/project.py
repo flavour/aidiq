d319fe90c5e4c633ef2413d52bdff88834527bb2
Fran Boon <fran@aidiq.com>
2012-01-13 00:48:39 +0000

    Timeline for gCalendar

--- controllers/project.py
+++ controllers/project.py
@@ -718,6 +718,139 @@ $('#submit_record__row input').click(function(){$('#comment-form').hide();$('#pr
 
 
 # =============================================================================
+def timeline():
+    """ Show the Project Calendar as a Simile Timeline """
+
+    calendar = deployment_settings.project.calendar
+    if not calendar:
+        response.error = T("Calendar not configured in models/000_config.py")
+        calendar = ""
+
+    # Create the DIV
+    item = DIV(_id="s3timeline", _style="height: 400px; border: 1px solid #aaa; font-family: Trebuchet MS, sans-serif; font-size: 85%;")
+
+    # Add core Simile Code
+    s3.scripts.append("/%s/static/scripts/simile/timeline/timeline-api.js" % request.application)
+    #s3.scripts.append("http://simile.mit.edu/timeline/api/timeline-api.js")
+
+    # Add our code to instantiate Simile
+    code = "".join(("""
+S3.timeline = Object();
+function loadGDataCallback(json) {
+    var entries = json.feed.entry;
+    var events = new Array()
+    for (var i = 0; i < entries.length; ++i) {
+        var entry = entries[i];
+        if ( entry["gd$when"] == null ) continue;
+        var when = entry["gd$when"][0];
+        var start = convertFromGDataDate(when.startTime);
+        var end = convertFromGDataDate(when.endTime);
+        var title = entry.title.$t;
+        var description = entry.content.$t;
+        var link = entry.link[0].href;
+        var event = {'start': start,
+                     'end': end,
+                     'title': title,
+                     'description': description,
+                     'link': link
+                    }
+        events.push(event)
+    }
+    S3.timeline.data = {'dateTimeFormat': 'iso8601',
+                        'events': events
+                        }
+    var url = '.';
+    S3.timeline.eventSource.loadJSON(S3.timeline.data, url);
+    S3.timeline.tl.hideLoadingMessage();
+    S3.timeline.tl.layout();
+};
+function zeroPad(n) {
+  if (n < 0) throw new Error('n is negative');
+  return (n < 10) ? '0' + n : n;
+}
+function convertToGDataDate(/*Date*/ date) {
+  return date.getFullYear() + '-' +
+         zeroPad(date.getMonth() + 1) + '-' +
+         zeroPad(date.getDate());
+}
+function convertFromGDataDate(/*string<YYYY-MM-DD>*/ date) {
+  var match = date.match(/(\d{4})-(\d{2})-(\d{2})/);
+  return new Date(parseInt(match[1], 10), parseInt(match[2], 10) - 1, parseInt(match[3], 10));
+}
+
+S3.timeline.onLoad = function() {
+    var tl_el = document.getElementById("s3timeline");
+    S3.timeline.eventSource = new Timeline.DefaultEventSource();
+    var theme = Timeline.ClassicTheme.create();
+    theme.event.bubble.width = 320;
+    theme.event.bubble.height = 180;
+
+    // centering the timeline three months previous makes it look nicer on load
+    var threeDaysFromNow = new Date(((new Date).getTime()) + 3 * 24 * 60 * 60 * 1000);
+    var bandInfos = [
+        Timeline.createBandInfo({
+            eventSource:    S3.timeline.eventSource,
+            date:           threeDaysFromNow,
+            width:          "40%", 
+            intervalUnit:   Timeline.DateTime.WEEK, 
+            intervalPixels: 300,
+            theme:          theme
+        }),
+        Timeline.createBandInfo({
+            eventSource:    S3.timeline.eventSource,
+            date:           threeDaysFromNow,
+            width:          "60%", 
+            intervalUnit:   Timeline.DateTime.MONTH, 
+            intervalPixels: 550,
+            theme:          theme
+        })
+    ];
+    bandInfos[1].syncWith = 0;
+    bandInfos[1].highlight = true;
+    S3.timeline.tl = Timeline.create(tl_el, bandInfos);
+
+    S3.timeline.tl.showLoadingMessage();
+    // Atom feed from a Google calendar
+    var feedUrl = '""", calendar, """';
+    var startDate = new Date((new Date).getDate());
+    var endDate = new Date(((new Date).getTime()) + 3 * 30 * 24 * 60 * 60 * 1000);
+    var getParams = '?start-min=' + convertToGDataDate(startDate) +
+                    '&start-max=' + convertToGDataDate(endDate) +
+                    '&alt=json-in-script' +
+                    '&callback=loadGDataCallback' +
+                    '&max-results=5000'; // choose 5000 as an arbitrarily large number
+    feedUrl += getParams;
+    var scriptTag = document.createElement('script');
+    scriptTag.src = feedUrl;
+    document.body.appendChild(scriptTag);
+}
+S3.timeline.resizeTimerID = null;
+S3.timeline.onResize = function() {
+    if (S3.timeline.resizeTimerID == null) {
+        S3.timeline.resizeTimerID = window.setTimeout(function() {
+            S3.timeline.resizeTimerID = null;
+            S3.timeline.tl.layout();
+        }, 500);
+    }
+}"""))
+
+    s3.js_global.append(code)
+    s3.jquery_ready.append("""
+$(window).load(function() {
+    SimileAjax.History.enabled = false;
+    S3.timeline.onLoad();
+});
+$(window).resize(function() {
+    S3.timeline.onResize();
+});""")
+
+    title = T("Project Calendar")
+
+    output = dict(title=title, item=item)
+    response.view = "timeline.html"
+    return output    
+
+# =============================================================================
 # Deprecated
 # =============================================================================
 def site_rheader(r):
